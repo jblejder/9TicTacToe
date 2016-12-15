ce3dd5a25ad0e675b791409111cf0ae56ac31134
Jakub Blejder <jakub.blejder@angrynerds.pl>
2016-12-15 14:47:23 +0100

    init project

---
+++ TicTacToe9/app/src/main/java/com/kutapps/tictactoe9/MainActivity.java
@@ -0,0 +1,15 @@
+package kutapps.com.tictactoe9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
